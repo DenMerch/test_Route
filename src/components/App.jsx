6ef99ab217bdd311fb494109eb802e625a165874
DenMerch <rosignolli@gmail.com>
2023-03-27 22:14:23 +0300

    rout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/about";
+import { Products } from "./pages/products";
+import { NotFound } from "./pages/NotFound";
+import styled from "styled-components";
+
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
